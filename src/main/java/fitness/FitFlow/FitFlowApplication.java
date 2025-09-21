7ddd856a3a9efc0fef95909c5f451dbb1a1056de
Tanya <tanyasinghrajput555@gmail.com>
2025-09-21 11:16:00 +0530

    I added system.out.println

--- src/main/java/fitness/FitFlow/FitFlowApplication.java
+++ src/main/java/fitness/FitFlow/FitFlowApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FitFlowApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Application Started");
+
 		SpringApplication.run(FitFlowApplication.class, args);
 	}
 
